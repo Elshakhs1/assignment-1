70fb3cedc712238a922ffd5a886f8d20295896e9
Islam Elshakhs <91914143+Elshakhs1@users.noreply.github.com>
2022-01-05 14:11:48 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,26 @@
-﻿using System;
-using System.Linq;
+﻿
+using System;
 
-namespace assignment_3
+namespace email_validation
 {
     class Program
     {
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
         static void Main(string[] args)
         {
-
-            var prices = new[] { 12, 15, 30, 10, 5 , 20};
-
-
-
-            var max = prices[0];
-            for (int i = 1; i < prices.Length; i++)
-            {
-                if (prices[i] > max)
-                {
-                    max = prices[i];
-                }
-            }
-            Console.WriteLine(max);
-
-            var min = prices[0];
-            for (int i = 1; i < prices.Length; i++)
-            {
-                if (prices[i] < min)
-                {
-                    min = prices[i];
-                }
-            }
-            Console.WriteLine(min);
-
-
-            var sum = 0d; // d for double
-            for (int i = 0; i < prices.Length; i++)
-            {
-                sum += prices[i];
-            }
-            Console.WriteLine(sum);
-            for (int i = 0; i < prices.Length; i++)
-            {
-
-
-            }
-
-            var average = prices.Average();
-            Console.WriteLine(average);
-
-
-
+            bool isOk = IsValidEmail("islamelshakhs@gmail.com");
+            Console.WriteLine(isOk);
         }
-
-
     }
 }
